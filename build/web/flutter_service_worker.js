4839d7f14b7d404bf13c13e927e964d08c52e111
Lars <lars.ole.heggland@gmail.com>
2022-05-09 21:19:49 +0200

    build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3b0289f13d944304295f0403cfc7dde0",
-"/": "3b0289f13d944304295f0403cfc7dde0",
-"main.dart.js": "2581695d5d86dea763902b3bb4a52d50",
+"index.html": "fffaf9fd132b123dcb53e1a7d29246fd",
+"/": "fffaf9fd132b123dcb53e1a7d29246fd",
+"main.dart.js": "734b5a78b5a37ac479c77ce1434918c6",
 "manifest.json": "addae12123fcf874600cd1bcfb8af5d4",
 "version.json": "dd289c07368e6092d42f8bd3778108b8"
 };
